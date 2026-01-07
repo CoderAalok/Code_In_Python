2497bba02f1f42e53f8e3cc33fc71e7fd1f9a4b1
yadavaalok565@gmail.com <yadavaalok565@gamil.com>
2026-01-07 07:05:33 +0545

    Capcha.py

---
+++ 34_Capcha.py
@@ -0,0 +1,7 @@
+import random 
+import string
+def captcha_gen(length=6):
+    alpha = string.ascii_letters + string.digits
+    Captcha = ''.join(random.choices(alpha,k=length))
+    return Captcha
+print(captcha_gen())
