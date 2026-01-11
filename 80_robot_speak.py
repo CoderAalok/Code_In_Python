a9db3caa5229ea6e6bd6c53082289f5251be6833
CoderAalok <yadavaalok565@gmail.com>
2026-01-12 02:46:36 +0545

    robot_speak

---
+++ 80_robot_speak.py
@@ -0,0 +1,17 @@
+# import pyttsx3
+# speak = pyttsx3.init()
+# while True:
+#     text = "Hey this is you working time."
+    
+#     speak.say("Phonk.mp3")
+#     speak.runAndWait()
+
+
+# import pygame
+
+# pygame.init()
+
+# pygame.mixer.music.load('Phonk.mp3')
+
+
+# pygame.mixer.music.play
