051a4ba6d7ced08554cac902e59dfd324c37eca3
CoderAalok <yadavaalok565@gmail.com>
2026-06-16 17:55:47 +0545

    remove greet and mod modules

--- greet.py
+++
@@ -1,6 +0,0 @@
-def addTwoNums(num1, num2):
-    return num1 + num2
-    
-if __name__ == "__main__":
-    add = addTwoNums(10,45)
-    print(add)
--- mod.py
+++
@@ -1,3 +0,0 @@
-import greet
-add = greet.addTwoNums(12,34)
-print(add)
