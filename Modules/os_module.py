4a5403e39f5c27ec50992dfc7b581f5be7997f1c
CoderAalok <yadavaalok565@gmail.com>
2026-06-16 17:49:47 +0545

    os_module

--- Modules/os_module.py
+++ Modules/os_module.py
@@ -1,16 +1,35 @@
-# os module is built-in module in python, use to operate the any file
+# import sys
+# print(dir(sys))
 
+
+
+# os module is built-in module in python, use to operate the any file
 import os
+import pathlib
 
 # print(dir(os))
+
 # os.rename("selling.log", 'selling_items.log')
-# current working directory
-# print((os.getcwd()))
+
+# change directory path
 # os.chdir("C://")
 # print(os.getcwd())
 
  #It check whether the file exist
 # print(os.access(file, os.W_OK)) 
 
-import sys
-print(dir(sys))
+# Main current working directory
+print((os.getcwd()))
+
+# this give current working directory instead of main directory
+base_dir = os.path.dirname(os.path.abspath(__file__))
+print(base_dir)
+
+file_path = os.path.join(base_dir, "Metacharacter.txt")
+
+if os.path.exists(file_path):
+    print(True)
+else:
+    print(False)
+
+print(pathlib.Path(__file__))
