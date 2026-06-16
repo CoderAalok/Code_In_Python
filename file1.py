c021afb5caa29d702b6d6dca074eb55a22644757
CoderAalok <yadavaalok565@gmail.com>
2026-06-16 17:54:45 +0545

    addTwoNums function and example usage

---
+++ file1.py
@@ -0,0 +1,6 @@
+def addTwoNums(num1, num2):
+    return num1 + num2
+    
+if __name__ == "__main__":
+    add = addTwoNums(10,45)
+    print(add)
---
+++ file2.py
@@ -0,0 +1,3 @@
+import file1
+add = file1.addTwoNums(12,34)
+print(add)
