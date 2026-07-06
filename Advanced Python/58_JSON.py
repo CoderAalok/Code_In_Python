d9315857ae59ddaa43581207dc70cce77729dc13
CoderAalok <yadavaalok565@gmail.com>
2026-07-06 15:44:44 +0545

    Refactor JSON handling to improve data structure and file operations

--- Advanced Python/58_JSON.py
+++ Advanced Python/58_JSON.py
@@ -3,7 +3,7 @@ import json
 # convert JSON into python dictionary(Deserialization)
 # data = '{"files":["music","photos"],"fruits":["banana","apple","coconut"]}'
 # print(data) # by this way can't access value using key
-data = '{"files":["music","photos"],"fruits":["banana","apple","coconut"]}'
+# data = '{"files":["music","photos"],"fruits":["banana","apple","coconut"]}'
 # # print(data) # by this way can't access value using key
 # result = json.loads(data)
 # print(result)
@@ -13,31 +13,35 @@ data = '{"files":["music","photos"],"fruits":["banana","apple","coconut"]}'
 # print(result['files'])
 # print(type(result))
 
+data = {
+    "Computer":"vision",
+    "Compitative":"Programming",
+    "Data Analytics":"Data Sciencetist",
+    "Artificial Intellegence": "Machine Learning",
+    "Loop Engineering": "casche memory"
+}
 # ======================================================================
+from pathlib import Path
+BASE = Path(__file__).parent
+file = BASE/"data.json"
+
+# Create and write json file
+# with open(file,"w")as f:
+#     json.dump([data], f, indent=4)
+
+
 
 # Read JSON from file
-with open("data.json","r")as f:
-    add_data = (json.load(f))
-# with open("data.json","r")as f:
-#     add_data = (json.load(f))
+with open(file,"r")as f:
+    add_data = json.load(f)
 
-# ======================================================================
-    
-# with open("data.json","r")as f:
 
-# with open("data.json","r")as f:
-# data = {
-#     "Computer":"vision",
-#     "Compitative":"Programming",
-#     "Data Analytics":"Data Sciencetist"
-#     "Data Analytics":"Data Sciencetist",
-#     1: False
-# }
+add_data.append(data)
+with open(file,"w")as f:
+    json.dump(add_data, f, indent=4)
+
 # add_data.append(data)
 # with open("data.json","w+")as f:
 #     print(json.dump(add_data, f, indent=4))
-# # add_data.append(data)
-# # with open("data.json","w")as f:
-# #     json.dump(add_data, f, indent=4)
 
 # print(int(list(add_data[1].keys())[-1])+123)
