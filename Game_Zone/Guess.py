a7c95a8ca73e8c30966a35934d89691755f76e3e
CoderAalok <yadavaalok565@gmail.com>
2026-07-29 08:17:56 +0545

    Refactor Guess game logic for improved user interaction and clarity

--- Game_Zone/Guess.py
+++ Game_Zone/Guess.py
@@ -1,4 +1,7 @@
 import random
+import time
+
+from regex import R
 def twice(final):
     result = final
     while True:
@@ -6,22 +9,18 @@ def twice(final):
         val = random.randint(1,100)
         val_1 = val
         try:
-            user = int(input(f" ->Take ({val}) and 1.Add or 2.Subtract or 3.Skip and Show Result -> "))
+            user = int(input(f"-> Can you ({val}):\n1. Add\n2. Subtract\n3. show result -> "))
             if user == 1:
                 result += val_1
-                continue
 
             elif user == 2:
                 result -= val_1
-                continue
-
-            elif user == 3:
-                return result
+                
             else:
-                print("🆒: I say choose either 1, 2, or 3 only!")
-
+                return result
+                
         except ValueError:
-            print("😶‍🌫️ Choose only 1, 2, or 3")
+            print("😶‍🌫️ Choose only 1, 2 & 3")
 
 def Guess_Number(num):
     result = 0
@@ -30,44 +29,45 @@ def Guess_Number(num):
 
     # length measure of number
     digit_size = len(num)
-    if not digit_size in (2,3):
-        print("\n⚠️ Hey my fellow, Only 2-digits or 3-digits guess 🙂‍↕️.")
+    if not digit_size in [2,3]:
+        print("⚠️ Hey my fellow, Only 2-digits or 3-digits guess 🙂‍↕️.\n")
         return
 
-    print("\nNow '⏪' reverse it's digits...")
+    print(">> Now '⏪' reverse it's digits...\n")
     rev_num = num[::-1] #1
-    input("\nIf you reversed '⏪'-> Say:'Done'✅ : ").capitalize().strip()
-
-    print(">>\nNow '➖' subtract: (reversed_number) and (your guessed_number) number...")
-    diff = abs(int(rev_num) - int(num)) #2
-    input("\nIf you subtracted '➖'-> Say:'Done'✅: ").capitalize().strip()
-
-    print(">>\nNow again '⏪' reverse subtracted' number...")
-    rev_diff = int(str(diff)[::-1]) #3
-    input("\nIf you reversed '⏪'-> Say:'Done'✅: ").capitalize().strip()
+    input("Enter to next step ->\n ")
     
-
-    print("\n>>Let's see the result...🤩")
+    print(">> Now '➖' subtract: (reversed_number) and (your guessed_number) number...\n")
+    diff = abs(int(rev_num) - int(num)) #2
+    input("Enter to next step ->\n ")
+    
+    print(">> Now again '⏪' reverse subtracted' number...\n")
+    rev_diff = int(str(diff)[::-1]) #3
+    input("Enter to next step ->\n ")
+    
+    print(">> Now add current reversed number and its non-reversed\n")
     result = rev_diff + diff
-
+    
+    print(">> Checking....🤩\n")
     # when reversed of 3-digits becomes 2-digits only executes
     if digit_size == 3 and result != 1089 or digit_size == 2 and result != 99:
         new_result = '0'+str(rev_diff)
         rev_ = int(new_result[::-1])
         result = rev_ + int(new_result)
 
-    print(">>....\nWait! Wait!! Wait!!!🤔 Before see the result")
+    time.sleep(2)
+    
+    print("Wait! Wait!! Wait!!!")
     outcome = twice(result)
     return outcome
 
 # Starting to guess
 num = input("🧏 Guess a number in your mind (2-digits) or (3-digits):\n")
 
-input("\nIf you guessed. -> Say: 👍'Done' :  ").capitalize().strip()
 print(f"\n🥴 So, Is your calculated value is {Guess_Number(num)}?")
 
-user_feedback = input("\nIf yes! 💬 Say:'Yes': ").capitalize().strip()
-if user_feedback == 'Yes':
+user_feedback = input("\nIf yes! 💬 Say:'Yes': ").strip().lower()
+if user_feedback == 'yes':
     print("\nThanks! For playing 😎.")
 else:
     print("\n🤦: Make ensure your all steps calculation correctly.")
